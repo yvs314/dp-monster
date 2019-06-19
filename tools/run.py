3adc985878f73d82d4c9cf112f918b2258ba57cb
yvs314 <yvs314@gmail.com>
2019-06-19 16:01:51 +0500

    Added "largish" group (.5–1.5GB) aimed at OMP testing to the runner.

--- tools/run.py
+++ tools/run.py
@@ -7,7 +7,8 @@ tasks = {
     "all" : ["ESC07.sop","ESC11.sop","ESC12.sop","ESC25.sop","ESC47.sop","ESC63.sop","ESC78.sop","br17.10.sop","br17.12.sop","ft53.1.sop","ft53.2.sop","ft53.3.sop","ft53.4.sop","ft70.1.sop","ft70.2.sop","ft70.3.sop","ft70.4.sop","kro124p.1.sop","kro124p.2.sop","kro124p.3.sop","kro124p.4.sop","p43.1.sop","p43.2.sop","p43.3.sop","p43.4.sop","prob.100.sop","prob42.sop","rbg048a.sop","rbg050c.sop","rbg109a.sop","rbg150a.sop","rbg174b.sop","rbg253a.sop","rbg323a.sop","rbg341a.sop","rbg358a.sop","rbg378a.sop","ry48p.1.sop","ry48p.2.sop","ry48p.3.sop","ry48p.4.sop"],
     "130GB" : ["ESC07.sop","ESC11.sop","ESC12.sop","ESC25.sop","br17.10.sop","br17.12.sop","ft53.4.sop","ft70.4.sop","p43.3.sop","p43.4.sop","rbg109a.sop","rbg150a.sop","rbg174b.sop","rbg253a.sop","ry48p.3.sop","ry48p.4.sop"],
     "4gb-300sec" : ["ESC07.sop","ESC11.sop","ESC12.sop","ESC25.sop","br17.10.sop","br17.12.sop","ft53.4.sop","ft70.4.sop","p43.4.sop","rbg109a.sop","rbg150a.sop","rbg174b.sop","rbg253a.sop","ry48p.4.sop"],
-    "5sec" : ["ESC07.sop","ESC11.sop","ESC12.sop","br17.10.sop","br17.12.sop","ft53.4.sop","p43.4.sop","rbg109a.sop","rbg150a.sop","ry48p.4.sop"]
+    "5sec" : ["ESC07.sop","ESC11.sop","ESC12.sop","br17.10.sop","br17.12.sop","ft53.4.sop","p43.4.sop","rbg109a.sop","rbg150a.sop","ry48p.4.sop"],
+    "largish": ["ESC25.sop","ft70.4.sop","rbg174b.sop","rbg253a.sop"]
 }
 
 if __name__ == "__main__":
