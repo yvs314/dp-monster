4bd5f8fe029b617da36abadab5d534e12472ed10
AndreySheka <AndreySheka@gmail.com>
2019-08-21 15:33:12 +0500

    aggregate all experiments with different prefixes in one file

--- tools/agg.py
+++ tools/agg.py
@@ -2,19 +2,12 @@ import argparse
 import pandas as pd
 import os
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='DPM project aggregator')
-    parser.add_argument('--out_dir', '-o', type=str, default=None,  help='Output directory. If not specified, ../results is used.')
-    parser.add_argument('--in_dir', '-i', type=str, default=None,  help='Input directory. If not specified, ../results is used.')
-    parser.add_argument('--file', '-f', type=str, default=None,  help='Output filename stored in output directory. If not specified, result.[EXT] is used.')
-    parser.add_argument('--type', '-t', type=str, default="xlsx", choices=["xlsx","csv"],  help='Output filename format. If not specified, xlsx is used')
-    parser.add_argument('--prefix', type=str, default="", help='Prefix run directories')
-    args = parser.parse_args()
 
-
-    in_dir = os.path.join("..", "results") if args.in_dir is None else in_dir
-    out_dir = os.path.join("..", "results") if args.out_dir is None else out_dir
-    out_file_name = "result" if args.file is None else args.file
+def aggregate(args):
+    # in_dir = os.path.join("..", "results") if args.in_dir is None else in_dir
+    # out_dir = os.path.join("..", "results") if args.out_dir is None else out_dir
+    in_dir, out_dir = args.in_dir, args.in_dir if args.out_dir is None else args.out_dir
+    out_file_name = "%sresult" % args.prefix if args.file is None else args.file
     out_file_name += '.' + args.type
 
     col_names = ["task_name", "problem_type", "direction", "method"]
@@ -36,7 +29,7 @@ if __name__ == "__main__":
             param[-1] = '.'.join(param[-1].split('.')[:-1])
             log_file_name = os.path.join(run_dir, log_name)
             dump_file_name = log_file_name[:-3] + 'dump'
-            param = dict(zip(range(1, len(param)+1), param))
+            param = dict(zip(range(1, len(param) + 1), param))
 
             if os.path.isfile(dump_file_name):
                 with open(dump_file_name, "r") as f:
@@ -48,6 +41,7 @@ if __name__ == "__main__":
                     param["time%s" % i] = float(fl.split("TOTAL DURATION IN SECONDS:").pop().splitlines()[0])
                     param["RAM%s" % i] = fl.split("RAM USAGE AT LAST LAYER:")[-1].splitlines()[0].split('~')[0].strip()
             return param
+
         param_logs = list(map(extract_param, run_logs))
         new_df = pd.DataFrame(param_logs)
         if df is None:
@@ -61,11 +55,53 @@ if __name__ == "__main__":
     ram_columns = sorted(list(filter(lambda s: isinstance(s, str) and s.startswith('RAM'), df.columns)))
     # df[avg_ram_col] = df[ram_columns].mean(axis=1, skipna=True)
 
-    df.columns = list(map(lambda c: c if (not isinstance(c, int)) or (c > len(col_names)) else col_names[c-1], df.columns))
-    not_run_cols = list(filter(lambda s: not(isinstance(s, str) and s.startswith('time')) and s != avg_time_col, df.columns))
+    df.columns = list(
+        map(lambda c: c if (not isinstance(c, int)) or (c > len(col_names)) else col_names[c - 1], df.columns))
+    not_run_cols = list(
+        filter(lambda s: not (isinstance(s, str) and s.startswith('time')) and s != avg_time_col, df.columns))
 
     df = df[not_run_cols + [avg_time_col] + time_columns]
     if args.type == "xlsx":
         df.to_excel(out_file_name, index=False)
     if args.type == "csv":
         df.to_csv(out_file_name, index=False, sep=';')
+    return df
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='DPM project aggregator')
+    parser.add_argument('--in_dir', '-i', type=str, default=os.path.join("..", "results"),
+                        help='Input directory. If not specified, ../results is used.')
+    parser.add_argument('--out_dir', '-o', type=str, default=None,
+                        help='Output directory. If not specified, --in_dir is used.')
+    parser.add_argument('--file', '-f', type=str, default=None,
+                        help='Output filename stored in output directory. If not specified, result.[EXT] is used.')
+    parser.add_argument('--type', '-t', type=str, default="xlsx", choices=["xlsx", "csv"],
+                        help='Output filename format. If not specified, xlsx is used')
+    parser.add_argument('--prefix', type=str, default=None, help='Prefix run directories')
+    args = parser.parse_args()
+
+    if args.prefix is not None:
+        aggregate(args)
+        exit(0)
+
+    # scan all available prefixes
+    in_dir = args.in_dir
+    exp_dirs = filter(lambda d: os.path.isdir(os.path.join(in_dir, d)), os.listdir(in_dir))
+    prefixes = set(map(lambda s: 'run'.join(s.split('run')[:-1]), exp_dirs))
+
+    dfs = []
+    for prefix in sorted(prefixes):
+        args.prefix = prefix
+        df = aggregate(args)
+        df.insert(loc=0, column='pref', value=prefix)
+        dfs.append(df)
+    dfs = pd.concat(dfs)
+
+    out_dir = args.in_dir if args.out_dir is None else args.out_dir
+    out_file_name = os.path.join(out_dir, "results." + args.type)
+
+    if args.type == "xlsx":
+        dfs.to_excel(out_file_name, index=False)
+    if args.type == "csv":
+        dfs.to_csv(out_file_name, index=False, sep=';')
