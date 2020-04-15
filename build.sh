31eb49d28a756f70d4491adb29c7b8f8a5f4645f
Yaroslav Salii <yvs314@gmail.com>
2020-04-15 18:31:15 -0400

    started on macOS-compat
    See, Apple Clang (default) has some problems with OMP, so I did the quick and dumb thing and switched to `clang 9.0`, available through `MacPorts`, which is installed by default into
    `/opt/local/libexec/llvm-9.0/bin/`
    It's still bad, I am really not interested in adding a patch to count the memory bytes on macOS/darwin. Though the error message was actually only about time.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 rm -rf build
 mkdir build
 cd build
+#AppleClang has problems with omp; try clang 9.0 through llvm (MacPorts)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	export CC="/opt/local/libexec/llvm-9.0/bin/clang"
+	export CXX="/opt/local/libexec/llvm-9.0/bin/clang++"
+fi
 cmake -DCMAKE_BUILD_TYPE=Release ../src
 make
