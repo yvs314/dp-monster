51830fdc38ae4e22eb7b3b326adb953fec17d49f
Yaroslav Salii <yvs314@gmail.com>
2020-04-20 12:24:27 -0400

    start storing exp.scripts; refit batch fab to comprehension
    Hey, it's cool to store experiment scripts! The *reasonable* way is to make a branch for *each* experiment I think, but I am just checking for now, so all's in `t-soplib06` branch. Then the flow could be (1)build dpm (2)run the `./exp-script/exp.py` (3) execute the `batch.csv` it produces.
    In addition, I refitted the batch fab script into a neat *list comprehension*. Might also set all the parameters WITHIN the list, but we'll see what is better.

---
+++ exp-script/exp6-hRtDP.py
@@ -0,0 +1,86 @@
+import csv #output
+#t-SOPLIB06 file names, sorted
+ns = ['R.200.100.1.sop',
+ 'R.200.100.15.sop',
+ 'R.200.100.30.sop',
+ 'R.200.100.60.sop',
+ 'R.200.1000.1.sop',
+ 'R.200.1000.15.sop',
+ 'R.200.1000.30.sop',
+ 'R.200.1000.60.sop',
+ 'R.300.100.1.sop',
+ 'R.300.100.15.sop',
+ 'R.300.100.30.sop',
+ 'R.300.100.60.sop',
+ 'R.300.1000.1.sop',
+ 'R.300.1000.15.sop',
+ 'R.300.1000.30.sop',
+ 'R.300.1000.60.sop',
+ 'R.400.100.1.sop',
+ 'R.400.100.15.sop',
+ 'R.400.100.30.sop',
+ 'R.400.100.60.sop',
+ 'R.400.1000.1.sop',
+ 'R.400.1000.15.sop',
+ 'R.400.1000.30.sop',
+ 'R.400.1000.60.sop',
+ 'R.500.100.1.sop',
+ 'R.500.100.15.sop',
+ 'R.500.100.30.sop',
+ 'R.500.100.60.sop',
+ 'R.500.1000.1.sop',
+ 'R.500.1000.15.sop',
+ 'R.500.1000.30.sop',
+ 'R.500.1000.60.sop',
+ 'R.600.100.1.sop',
+ 'R.600.100.15.sop',
+ 'R.600.100.30.sop',
+ 'R.600.100.60.sop',
+ 'R.600.1000.1.sop',
+ 'R.600.1000.15.sop',
+ 'R.600.1000.30.sop',
+ 'R.600.1000.60.sop',
+ 'R.700.100.1.sop',
+ 'R.700.100.15.sop',
+ 'R.700.100.30.sop',
+ 'R.700.100.60.sop',
+ 'R.700.1000.1.sop',
+ 'R.700.1000.15.sop',
+ 'R.700.1000.30.sop',
+ 'R.700.1000.60.sop']
+#=SEC.==PARAMETERS==========
+#set the principal parameters
+cores = [1] #hRtDP is fine with a single core
+drns=['FWD','BWD'] #both directions
+types=['TSP','BTSP'] #both problem types
+runs = 3 #run each instance 3 times
+pref = "exp6-hRtDP" #experiment name
+#set hRtDP's parameter range
+Hs = [10**x for x in range(6)] #1,10^1,...,10^5
+memDefault="2G" #should be enough even for R.700* at H=10^5
+
+#=SEC.==TASK==FABRICATION===
+#big whopping List Comprehension, one *dict* entry for each parameter set
+to_csv = [dict(
+    prefix=pref+"_%s" % nThr,
+    task=n,
+    threads=nThr,
+    d=d,t=t,H=H,
+    docker=1,
+    slurm=1,
+    mem=memDefault,
+    part='apollo',
+    time='1:0:0' #should be enough for hRtDP 
+)for n in ns
+ for nThr in cores 
+ for t in types 
+ for d in drns 
+ for H in Hs]
+#=SEC.==CSV==OUTPUT=========
+csv_file="%s.csv" % pref #.csv output named by the prefix
+csv_columns = to_csv[0].keys() #columns just as in the dict-list above
+#now write it 
+with open(csv_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=csv_columns,delimiter=';')
+    writer.writeheader()
+    writer.writerows(to_csv)
